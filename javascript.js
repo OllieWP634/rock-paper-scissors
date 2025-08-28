58d91e097119eafab0e213ad5fd3c87f7d1fec60
OllieWP634 <172636129+OllieWP634@users.noreply.github.com>
2025-08-28 17:30:08 +0100

    Create 4 variables to log scores and selections before a round is played

--- javascript.js
+++ javascript.js
@@ -18,3 +18,11 @@ function getHumanChoice() {
     // Prompts the user for a selection of rock, paper or scissors
     return prompt("Rock / Paper / Scissors?");
 }
+
+// Initialising human score and computer score at 0 before a round is played
+let humanScore = 0;
+let computerScore = 0;
+
+// Initialising two variables for the human input and computer generated move
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
