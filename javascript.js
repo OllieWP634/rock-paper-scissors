a88bb38224a2ae0438cdd738a20a66cd2cde0913
OllieWP634 <172636129+OllieWP634@users.noreply.github.com>
2025-09-07 19:08:00 +0100

    Add back playGame function, again without the for loop

--- javascript.js
+++ javascript.js
@@ -51,6 +51,14 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
+function playGame() {
+
+    // For loop calling playRound 5 times, updating humanSelection and computerSelection each loop
+    
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    
     // Determining final winner and scores of both user and computer
     if (humanScore > computerScore) {
         console.log("User score:", humanScore);
